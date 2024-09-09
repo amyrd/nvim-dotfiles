1a806e04b1ef0738716eebf198072a8377004706
amyrd <kuyaamyr@gmail.com>
2024-09-09 14:31:34 -0500

    update

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -164,6 +164,8 @@ return {
         cssls = {},
         html = {},
         tsserver = {},
+        sqls = {},
+
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
